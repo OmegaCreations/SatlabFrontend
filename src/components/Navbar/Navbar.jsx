8fea10020fb391a5ce6f638f5766963ae00949ea
Maksymilian Walicki <maxwalicki@gmail.com>
2025-01-05 12:31:22 +0100

    Setup menu navigation on page.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import style from "./Navbar.module.css";
+import { NavLink } from "react-router";
+
+const pages = ["Strona Główna", "O nas", "Kontakt"];
+const pagesLinks = ["", "about", "contact"];
+
+const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" className={style.navbar}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page, idx) => (
+                <NavLink
+                  key={page}
+                  to={`/${pagesLinks[idx]}`}
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${style.active} ${style.navItemSx}`
+                      : style.navItemSx
+                  }
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <p>{page}</p>
+                  </MenuItem>
+                </NavLink>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page, idx) => (
+              <NavLink
+                key={page}
+                to={`/${pagesLinks[idx]}`}
+                className={({ isActive }) =>
+                  isActive ? `${style.active} ${style.navItem}` : style.navItem
+                }
+              >
+                <Button onClick={handleCloseNavMenu}>
+                  <p>{page}</p>
+                </Button>
+              </NavLink>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar/Navbar";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* Redux provider */}
+    {/* Routing for application */}
+
+    <BrowserRouter>
+      {/* Navbar for all routes */}
+      <div className="navContainer">
+        <Navbar />
+      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* 
+          
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/article/:id" element={<Article />} /> */}
+        </Routes>
+      </main>
+    </BrowserRouter>
+  </StrictMode>
+);
